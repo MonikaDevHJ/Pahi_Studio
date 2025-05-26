359355b431975dec8e6dd2fa1d7414eaedf81d00
Monika H J <monikahj7@gmail.com>
2025-05-26 12:15:34 +0530

    making fahion page

--- src/app/services/fashion/page.tsx
+++ src/app/services/fashion/page.tsx
@@ -1,7 +1,22 @@
 export default function FashionPage() {
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-black">Fashion Page ✅</h1>
+    <div className="min-h-screen pt-28 bg-white flex  justify-center animate-zoom-in">
+      <div className="">
+        <div className="flex gap-5 mt-2" style={{ textShadow: "2px 2px 5px rgba(0,0,0,0.3)" }}>
+          <p className="text-5xl font-extrabold text-sky-500">Fashion</p>
+          <p className="text-5xl font-extrabold text-black">PhotoGraphy</p>
+        </div>
+
+
+
+
+
+        <div className="mt-7">
+          <p className="text-2xl font-bold text-gray-500">Photos are Your most important Marketing Tool</p>
+
+        </div>
+      </div>
+
     </div>
   );
 }
